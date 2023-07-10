41120e238c5e7727e2d3966ee94124dd7b8d96d3
lecav <lecav30@outlook.com>
2023-07-10 11:55:13 -0500

    Updates

---
+++ js/app.js
@@ -0,0 +1,83 @@
+bars = document.querySelector('.bars');
+bars.addEventListener('click', () => {
+    navBar = document.querySelector('.nav-bar');
+    navBar.classList.toggle('active');
+});
+
+backend = document.querySelector('#backend');
+backend.addEventListener('click', () => {
+    backend.classList.add('remove-hover');
+
+    techonologies = document.querySelector('#backend-techonologies');
+    techonologies.classList.toggle('hidden');
+
+    description = document.querySelector('#backend-description');
+    description.classList.toggle('hidden');
+
+    setTimeout(() => backend.classList.remove('remove-hover'), 1000);
+});
+
+frontend = document.querySelector('#frontend');
+frontend.addEventListener('click', () => {
+    frontend.classList.add('remove-hover');
+
+    techonologies = document.querySelector('#frontend-techonologies');
+    techonologies.classList.toggle('hidden');
+
+    description = document.querySelector('#frontend-description');
+    description.classList.toggle('hidden');
+
+    setTimeout(() => frontend.classList.remove('remove-hover'), 1000);
+});
+
+mobile = document.querySelector('#mobile');
+mobile.addEventListener('click', () => {
+    mobile.classList.add('remove-hover');
+
+    techonologies = document.querySelector('#mobile-techonologies');
+    techonologies.classList.toggle('hidden');
+
+    description = document.querySelector('#mobile-description');
+    description.classList.toggle('hidden');
+
+    setTimeout(() => mobile.classList.remove('remove-hover'), 1000);
+});
+
+const contactForm = document.querySelector('.contact-form');
+let name = document.getElementById('POST-name');
+let email = document.getElementById('POST-email');
+let subject = document.getElementById('POST-subject');
+let message = document.getElementById('POST-message');
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let formData = {
+        name: name.value,
+        email: email.value,
+        subject: subject.value,
+        message: message.value,
+    }
+
+    console.log(formData);
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('content-type', 'application/json');
+
+    xhr.onload = function () {
+        console.log(xhr.responseText);
+        if (xhr.responseText == 'success') {
+            alert('Email sent');
+            name.value = '';
+            email.value = '';
+            subject.value = '';
+            message.value = '';
+        } else {
+            alert('Something went wrong!');
+        }
+    }
+
+    xhr.send(JSON.stringify(formData));
+});
