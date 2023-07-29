420d6ec58155472096499d01e3a938692a9674f9
lecav <lecav30@outlook.com>
2023-07-29 18:32:44 -0500

    Updates

--- js/app.js
+++ js/app.js
@@ -1,83 +1,98 @@
-bars = document.querySelector('.bars');
-bars.addEventListener('click', () => {
-    navBar = document.querySelector('.nav-bar');
-    navBar.classList.toggle('active');
+const select = (el, all = false) => {
+  el = el.trim();
+  if (all) {
+    return [...document.querySelectorAll(el)];
+  } else {
+    return document.querySelector(el);
+  }
+};
+
+let bars = select(".bars");
+bars.addEventListener("click", () => {
+  navbar = select(".nav-bar");
+  navbar.classList.toggle("active");
 });
 
-backend = document.querySelector('#backend');
-backend.addEventListener('click', () => {
-    backend.classList.add('remove-hover');
+const onscroll = (el, listener) => {
+  el.addEventListener("scroll", listener);
+};
 
-    techonologies = document.querySelector('#backend-techonologies');
-    techonologies.classList.toggle('hidden');
+let navbar_links = select(".scrollto", true);
 
-    description = document.querySelector('#backend-description');
-    description.classList.toggle('hidden');
-
-    setTimeout(() => backend.classList.remove('remove-hover'), 1000);
-});
-
-frontend = document.querySelector('#frontend');
-frontend.addEventListener('click', () => {
-    frontend.classList.add('remove-hover');
-
-    techonologies = document.querySelector('#frontend-techonologies');
-    techonologies.classList.toggle('hidden');
-
-    description = document.querySelector('#frontend-description');
-    description.classList.toggle('hidden');
-
-    setTimeout(() => frontend.classList.remove('remove-hover'), 1000);
-});
-
-mobile = document.querySelector('#mobile');
-mobile.addEventListener('click', () => {
-    mobile.classList.add('remove-hover');
-
-    techonologies = document.querySelector('#mobile-techonologies');
-    techonologies.classList.toggle('hidden');
-
-    description = document.querySelector('#mobile-description');
-    description.classList.toggle('hidden');
-
-    setTimeout(() => mobile.classList.remove('remove-hover'), 1000);
-});
-
-const contactForm = document.querySelector('.contact-form');
-let name = document.getElementById('POST-name');
-let email = document.getElementById('POST-email');
-let subject = document.getElementById('POST-subject');
-let message = document.getElementById('POST-message');
-
-contactForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    let formData = {
-        name: name.value,
-        email: email.value,
-        subject: subject.value,
-        message: message.value,
+const navbar_links_active = () => {
+  let position = window.scrollY + 200;
+  navbar_links.forEach((navbar_link) => {
+    if (!navbar_link.hash) return;
+    let section = select(navbar_link.hash);
+    if (!section) return;
+    if (
+      position >= section.offsetTop &&
+      position <= section.offsetTop + section.offsetHeight
+    ) {
+      navbar_link.classList.add("active");
+    } else {
+      navbar_link.classList.remove("active");
     }
+  });
+};
 
-    console.log(formData);
+window.addEventListener("load", navbar_links_active);
+onscroll(document, navbar_links_active);
 
-    let xhr = new XMLHttpRequest();
+const card_toggle = (tech) => {
+  my_tech = select(`#${tech}`);
+  my_tech.addEventListener("click", () => {
+    my_tech.classList.add("remove-hover");
 
-    xhr.open('POST', '/');
-    xhr.setRequestHeader('content-type', 'application/json');
+    technologies = select(`#${tech}-technologies`);
+    technologies.classList.toggle("hidden");
 
-    xhr.onload = function () {
-        console.log(xhr.responseText);
-        if (xhr.responseText == 'success') {
-            alert('Email sent');
-            name.value = '';
-            email.value = '';
-            subject.value = '';
-            message.value = '';
-        } else {
-            alert('Something went wrong!');
-        }
+    description = select(`#${tech}-description`);
+    description.classList.toggle("hidden");
+
+    setTimeout(() => my_tech.classList.remove("remove-hover"), 1000);
+  });
+};
+
+card_toggle("backend");
+card_toggle("frontend");
+card_toggle("mobile");
+
+const contactForm = select(".contact-form");
+let name = select("#POST-name");
+let email = select("#POST-email");
+let subject = select("#POST-subject");
+let message = select("#POST-message");
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let formData = {
+    name: name.value,
+    email: email.value,
+    subject: subject.value,
+    message: message.value,
+  };
+
+  console.log(formData);
+
+  let xhr = new XMLHttpRequest();
+
+  xhr.open("POST", "/");
+  xhr.setRequestHeader("content-type", "application/json");
+
+  xhr.onload = function () {
+    console.log(xhr.responseText);
+    if (xhr.responseText == "success") {
+      alert("Email sent");
+      name.value = "";
+      email.value = "";
+      subject.value = "";
+      message.value = "";
+    } else {
+      alert("Something went wrong!");
     }
+  };
 
-    xhr.send(JSON.stringify(formData));
+  xhr.send(JSON.stringify(formData));
 });
